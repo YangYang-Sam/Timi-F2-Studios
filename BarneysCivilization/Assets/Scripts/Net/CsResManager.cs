8b605b436f203449f0ea3e1bdd18125d45444fb4
Anorak.G <2201363551@qq.com>
2020-09-28 11:49:51 +0800

    NET

---
+++ BarneysCivilization/Assets/Scripts/Net/CsResManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetTest
+{
+    class CsResManager
+    {
+        public static void ResHello(int result)
+        {
+            Console.WriteLine(result);
+        }
+        public static void ResLogin(int result)
+        {
+            Console.WriteLine(result);
+        }
+        public static void ResRegist(int result)
+        {
+            Console.WriteLine(result);
+        }
+        public static void NtyLoginOut(String uid, int reason)
+        {
+            //
+            Console.WriteLine(uid);
+        }
+        public static void ResLoginOut(int result)
+        {
+            //
+            Console.WriteLine(result);
+        }
+        public static void ResCreateRoom(int[] result)
+        {
+            //
+            Console.WriteLine(result);
+        }
+        public static void ResQuitRoom(int result)
+        {
+            //
+            Console.WriteLine(result);
+        }
+    }
+}
