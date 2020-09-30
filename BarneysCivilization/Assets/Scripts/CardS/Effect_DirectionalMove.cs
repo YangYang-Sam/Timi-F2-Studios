bf03e39726cdb481afb38fd49fc9fa354ac2cdf6
HeHe <1565624101@qq.com>
2020-09-30 21:25:02 +0800

    蔓延Effect

---
+++ BarneysCivilization/Assets/Scripts/CardS/Effect_DirectionalMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_DirectionalMove : CardEffect
+{
+    public override bool CanUseCard(CardManager user, HexCell cell)
+    {
+        return base.CanUseCard(user, cell);
+    }
+    public override List<HexCell> GetCanUseCells(CardManager user)
+    {
+        return base.GetCanUseCells(user);
+    }
+    public override void Effect(CardManager user, HexCell cell)
+    {
+        user.SetUnitMoveModeTemporarily(UnitMoveMode.Directional);
+    }
+}
