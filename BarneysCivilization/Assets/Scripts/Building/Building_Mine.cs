cbeeb1dd9c741aba91fd1e03a7c3b0f558f5a1e3
HeHe <1565624101@qq.com>
2020-09-30 16:36:18 +0800

    增加Building_Mine， 增加SupportCellType设置给Effect_CreateBuilding

---
+++ BarneysCivilization/Assets/Scripts/Building/Building_Mine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building_Mine : Building_Base
+{
+    public int ResourceAmount;
+
+    protected override void OnGameStateChange()
+    {
+        base.OnGameStateChange();
+
+        if (InGameManager.isGameState(GameStateType.AfterBattle))
+        {
+            Owner.TempResourceAmount += ResourceAmount;
+        }
+    }
+}
--- BarneysCivilization/Assets/Scripts/CardS/Effect_CreateBuilding.cs
+++ BarneysCivilization/Assets/Scripts/CardS/Effect_CreateBuilding.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class Effect_CreateBuilding : CardEffect
 {
+    public bool SupportAllCellTypes = true;
+    public List<HexCellType> SupportCellTypes = new List<HexCellType>();
     public int RequireHealth;
     public int DecreaseHealth;
     public GameObject BuildingPrefab;
 
     public override bool CanUseCard(CardManager user, HexCell cell)
     {
-        return base.CanUseCard(user, cell) && GetCanUseCells(user).Contains(cell);
+        bool isSupportCellType = SupportAllCellTypes ? true : SupportCellTypes.Contains(cell.CellType);
+        return base.CanUseCard(user, cell) && GetCanUseCells(user).Contains(cell) && isSupportCellType;
     }
+
     public override List<HexCell> GetCanUseCells(CardManager user)
     {
         List<HexCell> cells = new List<HexCell>();
         foreach (HexCell cell in user.OccupiedCells)
         {
-            if (cell.GetUnitOnCell().Health >= RequireHealth && cell.PlacedBuilding == null)
+            bool isSupportCellType = SupportAllCellTypes ? true : SupportCellTypes.Contains(cell.CellType);
+            if (cell.GetUnitOnCell().Health >= RequireHealth && cell.PlacedBuilding == null && isSupportCellType)
             {
                 cells.Add(cell);
             }
         }
         return cells;
     }
+
     public override void Effect(CardManager user, HexCell cell)
     {
         base.Effect(user, cell);
